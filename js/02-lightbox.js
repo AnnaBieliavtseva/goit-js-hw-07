9043c709a76367e37be259c676d20dce6d3580ad
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2022-08-11 23:22:17 +0200

    finish 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.addEventListener("click", onGalleryRefClick);
+
+function createGallery(images) {
+  const galleryImages = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+
+  return galleryImages;
+}
+
+galleryRef.insertAdjacentHTML("beforeend", createGallery());
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+// gallery.on("shown.simplelightbox", function () {
+//   // do something…
+// });
+
+function onGalleryRefClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
 
 console.log(galleryItems);
